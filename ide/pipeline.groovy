49441e05f047640922c32072dd3837c11524e66b
o.khytrov <o.khytrov@gmail.com>
2022-04-18 16:40:54 +0300

    add groovy pipeline

---
+++ ide/pipeline.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+               git branch: 'main', url: 'https://github.com/o-khytrov/jgsu-spring-petclinic.git' 
+            }
+        }
+        stage('Build') {
+            steps {
+                
+                 sh "./mvnw clean package"
+            }
+
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                always {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
